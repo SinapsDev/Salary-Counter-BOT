3e12896849deecd1dc0a9c5c2045da9b8f7ed90a
Sinaps <alaouirayan55@gmail.com>
2022-08-05 15:35:24 +0100

    better understanding of interactions + register employee commands and choices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { config } from 'dotenv';
 import { Client, Routes } from 'discord.js';    
 import { REST } from '@discordjs/rest';
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+
+import { LSPD_Roles } from './Enum/LSPD_Roles.js';
 
 config();
 
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
+const SHEET_ID = process.env.SHEET_ID
 
 const client = new Client({intents: ['Guilds', 'GuildMessages']});
 
@@ -18,6 +22,15 @@ client.on('ready', ()  => {
     console.log('Salary Bot by Sinaps is ON and ready to use!')
 })
 
+client.on('interactionCreate', (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        if (interaction.commandName === 'register_employee') {
+            console.log(interaction.options.get('nom'))
+            interaction.reply({content: 'Hey'});
+        }
+    }
+})
+
 async function main() {
     const commands = [
         {
@@ -27,6 +40,14 @@ async function main() {
         {
             name: 'duty_off',
             description: 'Vous déposez votre service avec cette commande.'
+        },
+        {
+            name: 'register_employee',
+            description: 'Enregistrer un nouveau employée dans la base de donnée du bot.',
+            options: [
+                {name: 'nom', description: 'Nom de l\'employée.', type: 6, required: true},
+                {name: 'grade', description: 'Grade de l\'employée.', type: 3, required: true, choices: LSPD_Roles},
+            ]
         }
     ]
 
