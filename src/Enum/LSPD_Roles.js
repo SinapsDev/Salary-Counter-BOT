fcf7d703ecc86eec9c831758ee0c7c81570f8aea
Sinaps <alaouirayan55@gmail.com>
2022-08-05 15:34:58 +0100

    created a lspd roles file as an enum

---
+++ src/Enum/LSPD_Roles.js
@@ -0,0 +1,18 @@
+export const LSPD_Roles = [
+    {
+        name: 'Recruit',
+        value: 'recruit'
+    },
+    {
+        name: 'Officer 1',
+        value: 'officer 1'
+    },
+    {
+        name: 'Officer 2',
+        value: 'officer 2'
+    },
+    {
+        name: 'Officer 3',
+        value: 'officer 3'
+    }
+]
